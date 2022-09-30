f5253142bf54204a64c142d6b778925d7ecc0063
Pablo Hernández <phernandezm07@gmail.com>
2022-09-29 22:59:01 -0500

    Funcionalidad completada

--- Hangedgame/game.js
+++ Hangedgame/game.js
@@ -5,11 +5,12 @@ const mainMenu=document.getElementById("mainMenu");
 //Game items
 const ctrls = document.getElementById("controls");
 const gc = document.getElementById("gameContainer");
-const cv = document.createElement("canvas");
+const cv = document.getElementById("canvas");
 const counter=document.getElementById("counter");
 const kb = document.getElementById("keyboard");
-let words = ["AUTO","CAMION","PlXEL","PANTALLA","FRUTA","NIÑOS","ESPACIO","TECLADO","AIRE","AVE","ZETA","SETA","HONGO","CELULAR","RAIZ"];
+let words = ["AUTO","CAMION","PlXEL","PANTALLA","FRUTA","NIÑOS","ESPACIO","TECLADO","AIRE","AVE","ZETA","SETA","HONGO","CELULAR","RAIZ", "ELEFANTE", "SUEÑO", "SOL", "LUNA", "MONEDA", "COLOR"];
 let attp;
+let wordstr;
 
 //Menu items
 const mc = document.getElementById("menuContainer");
@@ -29,6 +30,7 @@ const newWordInput = document.createElement("input");
 const newWordAdd = document.createElement("input");
 
 let inGame=true;
+
 init();
 
 function init(){   
@@ -49,6 +51,7 @@ function init(){
     keys.forEach(e => {
         let keybtn = document.createElement("input");
         setAttr(keybtn, {"type":"button", "class":"key", "value":e});
+        keybtn.addEventListener("click", ()=>{pressKey(keybtn)});
         kb.appendChild(keybtn);
     }); 
     
@@ -62,6 +65,7 @@ function init(){
     nwForm.appendChild(newWordAdd);
     setAttr(nwForm, {"action":"javascript:addWord();"})
     newWord.appendChild(nwForm);
+    draw();
 }
 
 function writeWord(){
@@ -70,11 +74,13 @@ function writeWord(){
     word.innerHTML="";
 
     let wordn = Math.floor(Math.random()*words.length);
-    let wordstr = words[wordn];
+    wordstr = words[wordn];
     for(let i=0; i<wordstr.length; i++){
+        let wC = document.createElement("p")
         let wordd = document.createElement("div");
         setAttr(wordd, {"class":"letter"});
-        wordd.innerText=wordstr[i];
+        wC.innerText="\u00a0";
+        wordd.appendChild(wC);
         word.appendChild(wordd);
     }
 }
@@ -89,6 +95,8 @@ function newGame(){
         ctrls.appendChild(mc);
     }
     writeWord();
+    draw();
+    document.querySelectorAll(".usedKey").forEach(toggleKey);
 }
 
 function setAttr(el, attr){
@@ -118,4 +126,92 @@ function addWord(){
 
     words.push(wrd);
     alert("Agregada: "+ wrd);
+}
+
+function toggleKey(el){
+    el.classList.toggle("usedKey");
+}
+
+function pressKey(el){
+    let isIn=false;
+    toggleKey(el);
+    for(let i=0; i<wordstr.length; i++){
+        if(wordstr[i]===el.value){
+            let letters = word.getElementsByClassName("letter");
+            letters[i].getElementsByTagName("p")[0].innerText=el.value;
+            isIn=true;
+            
+            let win=true;
+            for(let i=0; i<letters.length; i++){
+                if(letters[i].getElementsByTagName("p")[0].innerText=="\u00a0"){
+                    win=false;
+                }
+            }
+
+            if(win){
+                cAlert("¡Ganaste!", "Felicitaciones, has adivinado la palabra "+wordstr);
+            }
+        }
+    }
+
+    if(!isIn){
+        attp--;
+        counter.innerText=attp;
+        draw();
+    }
+
+    if(attp<=0){
+        cAlert("¡Has fallado!", "La palabra era "+wordstr);
+    }
+}
+
+function cAlert(title, message){
+    let cAdiv = document.createElement("div");
+    cAdiv.setAttribute("id", "cAlertC");
+    cAdiv.innerHTML='<div id=cAlert><div id="cAlertTitle">'+title+'</div><div id="cAlertMsg">'+message+'</div><input id="cAOk" type="button" value="Jugar de nuevo"></div>';
+    document.body.appendChild(cAdiv);
+    document.getElementById("cAOk").addEventListener("click", ()=>{
+        document.body.removeChild(cAdiv);
+        newGame();
+    });
+}
+
+function draw(){
+    let ctx = cv.getContext("2d");
+    ctx.fillStyle="#556067";
+    let h = cv.height-5;
+    let w = cv.width-5;
+    let pt=h*0.03;
+    let rp = w*0.5;
+    ctx.clearRect(0,0,w,h);
+
+    ctx.fillRect(w*0.2,0, pt, h);//Vertical
+    ctx.fillRect(w*0.2,0, w*0.3,pt);//Top-Horizontal
+    ctx.fillRect(w*0.5, 0, pt, h*0.2);//Rope
+    ctx.fillRect(w*0.1,h, w*0.4,pt);//Top-Horizontal
+    if(attp<6){
+        if (attp<6){
+            ctx.fillRect(rp-((pt*6)/2), h*0.2, pt*6, pt*4)//head
+        }
+
+        if(attp<5){
+            ctx.fillRect(rp-((pt*4)/2), h*0.2+pt*4, pt*4, pt*6)//body
+        }
+
+        if(attp<4){
+            ctx.fillRect(rp-(pt*7), h*0.2+pt*4, pt*7, pt*3)//leftArm
+        }
+
+        if(attp<3){
+            ctx.fillRect(rp+(pt*0.3), h*0.2+pt*4, pt*7, pt*3)//RightArm
+        }
+
+        if(attp<2){
+            ctx.fillRect(rp-(pt*5.5), h*0.2+pt*9, pt*4, pt*4)//leftLeg
+        }
+
+        if(attp==0){
+            ctx.fillRect(rp+(pt*1.3), h*0.2+pt*9, pt*4, pt*4)//RightLeg
+        }
+    }
 }
